bcc449c9ac8e8727373a9b17dbb6b78039451390
Nikilesh <nikilesh@astragaze.com>
2024-01-18 16:45:31 +0530

    Add project files.

---
+++ dotnet-webapp/Models/MenuItem.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace dotnet_webapp.Models
+{
+    public class MenuItem
+    {
+        public string Text { get; set; } = string.Empty;
+        public string Url { get; set; } = string.Empty;
+        public List<MenuItem>? Items { get; set; }
+    }
+}
